9373d188beafb206a68c37d696f7ced34b2e7fb4
Cedric Berthet <berthetcedric@gmail.com>
2023-10-18 16:12:23 +0200

    3.2

--- 03-colors/02-change-bcg-two/script.js
+++ 03-colors/02-change-bcg-two/script.js
@@ -11,4 +11,11 @@
 
 (() => {
     // your code here
+    let run = document.getElementById("run");
+    
+
+    run.addEventListener("click", () => {
+        let color = document.getElementById("color").value;
+        document.body.style.backgroundColor = color;
+    });
 })();
