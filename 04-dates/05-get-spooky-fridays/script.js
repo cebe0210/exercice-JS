031d61eb2dfeabe7324f470f6595dea978bff148
Cedric Berthet <berthetcedric@gmail.com>
2023-10-19 11:57:48 +0200

    4.5

--- 04-dates/05-get-spooky-fridays/script.js
+++ 04-dates/05-get-spooky-fridays/script.js
@@ -11,4 +11,27 @@
 
 (() => {
     // your code here
+    let run = document.getElementById("run");
+    let yearin = document.getElementById("year");
+
+    run.addEventListener("click", calcul);
+
+    function calcul (){
+        let year = parseInt(yearin.value);
+        let result = [];
+        for(month = 0; month < 12; month++){
+        let date = new Date(year, month, 13);
+        if(date.getDay() === 5){
+            result.push(date);
+        }
+        }
+        if (result.length > 0){
+            let resultat = result.map(date => date.toLocaleDateString('fr-FR')).join(", ");
+            alert("il y a " + result.length + " vendredi 13 en " + year + " : " + resultat);
+        }
+        else {
+            alert("Il n'y à aucun vendredi 13 en " + year);
+        }
+    }
+    
 })();
