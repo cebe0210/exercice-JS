3c82851ed51d3dbe9caa38a5f240a28c2791c8b5
Cedric Berthet <berthetcedric@gmail.com>
2023-10-19 11:15:44 +0200

    4.4

--- 04-dates/04-locale-date/script.js
+++ 04-dates/04-locale-date/script.js
@@ -12,4 +12,23 @@
 (() => {
     // to change the content of a tag: document.getElementById("element-id").innerHTML = "new-value"
     // your code here
+
+    function datedujour(){
+        let target = document.getElementById("target");
+        let date = new Date();
+        let jours = ["Dimanche", "Lundi", "Mardi", "Mercredi", "Jeudi", "Vendredi", "Samedi"];
+        let nomjour = jours[date.getDay()];
+        let options = {
+            year: 'numeric',
+            month: 'long',
+            day: 'numeric',
+            hour: '2-digit',
+            minute: '2-digit',
+            };
+        let format = date.toLocaleString('fr-FR', options);
+        format = format.replace(":", "h")
+
+        target.innerHTML = "Nous sommes le " + nomjour + " " + format;
+    }
+    datedujour();
 })();
