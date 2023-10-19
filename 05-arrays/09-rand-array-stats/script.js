85854f81e04ba495016129f29157ef4fb6a9ca11
Cedric Berthet <berthetcedric@gmail.com>
2023-10-19 15:55:34 +0200

    5.9

--- 05-arrays/09-rand-array-stats/script.js
+++ 05-arrays/09-rand-array-stats/script.js
@@ -11,4 +11,47 @@
 
 (() => {
     // your code here
+   
+    let run = document.getElementById("run");
+    let table = [];
+
+    function arrays (){
+
+    for (let i = 0; i < 10; i++){
+        let random = Math.floor(Math.random() * 100) +1;
+        table.push(random);
+        let nombres = document.getElementById("n-" + (i+1));
+        nombres.textContent = random;
+    }
+        let minimum = table[0];
+    for (let i = 1; i < table.length; i++){
+        if(table[i] < minimum){
+            minimum = table[i];
+        }
+        let min = document.getElementById("min");
+        min.textContent = minimum;
+    }
+    let maximum = table[0];
+    for (let i = 1; i < table.length; i++){
+        if(table[i] > maximum){
+            maximum = table[i];
+        }
+        let max = document.getElementById("max");
+        max.textContent = maximum;
+    }
+    let somme = 0;
+    for (let i = 0; i < table.length; i++){
+        somme += table[i];
+        sum = document.getElementById("sum");
+        sum.textContent = somme;
+    }
+    average = document.getElementById("average");
+    average.textContent = somme/table.length;
+
+
+
+    }
+   
+
+    run.addEventListener("click", arrays )
 })();
